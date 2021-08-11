7fadb89390d74236624b6246e20dc59b8994b289
Caroline Scherer <scherer.caroline@gmail.com>
2021-08-11 15:48:15 -0300

    modificando Strings para lowercase

--- GerCorrespondencia/src/view/InterfaceRegistrarEntrada.java
+++ GerCorrespondencia/src/view/InterfaceRegistrarEntrada.java
@@ -114,7 +114,7 @@ public class InterfaceRegistrarEntrada implements Comando {
             Processador.direcionar("0");
             return null;
         }
-        opcao = opcao.replaceAll("\\s{2,}", " ").trim();
+        opcao = opcao.replaceAll("\\s{2,}", " ").trim().toLowerCase();
         if (opcao.length() == 0) {
             throw new CampoVazioException(mensagem);
         } else {
